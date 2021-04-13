5592b70b7930af7c738a7721af2d9837a643403f
Putri Alivia <putrialivia666@gmail.com>
2021-04-13 14:57:11 +0700

    Pengeditan class Model Article

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Article extends Model
 {
     use HasFactory;
+    /**
+     * The atributes that are mass assignable
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title',
+        'content',
+        'featured_image'
+    ];
 }
